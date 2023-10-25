201113847a86d0c585fd6772b55f80188f28b3c5
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-10-25 21:09:22 +0200

    Update config.py

---
+++ eia_fast_app/config.py
@@ -0,0 +1,17 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    env_name: str = "Local"
+    base_url: str = "http://localhost:8000"
+    db_url: str = "sqlite:///./eia_app.db"
+
+    class Config:
+        env_file = ".env"
+
+def get_settings() -> Settings:
+    settings = Settings()
+    print(f"Loading settings for: {settings.env_name}")
+    return settings
+
+
+get_settings().db_url
