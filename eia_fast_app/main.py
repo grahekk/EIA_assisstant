0276000ca7b9394f0a9383a68696a55b7a5e3a35
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-10-25 22:28:08 +0200

    Update main.py

--- eia_fast_app/main.py
+++ eia_fast_app/main.py
@@ -1,7 +1,32 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from shapely.geometry import shape
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return "Welcome to the eia assistant API :)"
+
+class ShapefileData(BaseModel):
+    name: str
+    geometry_type: str
+
+
+@app.post("/upload_shapefile/")
+async def upload_shapefile(file: UploadFile):
+    if file.content_type != "application/octet-stream":
+        return {"error": "Invalid file format. Please upload a shapefile (SHP)."}
+    
+    with open(file.filename, 'wb') as shp_file:
+        shp_file.write(file.file.read())
+    
+    # Now you can work with the SHP file using libraries like geopandas and shapely.
+    
+    # Example: Reading shapefile and extracting some data
+    # import geopandas as gpd
+    # data = gpd.read_file(file.filename)
+    # feature = data.iloc[0]  # Get the first feature
+    # geometry_type = shape(feature['geometry']).geom_type
+
+    # return {"name": file.filename, "geometry_type": geometry_type}
