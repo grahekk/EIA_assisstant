7d4f012107030c4d96d95caf7f9477320ed7f3ce
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-10-30 06:51:50 +0100

    Update main.py

--- eia_fast_app/main.py
+++ eia_fast_app/main.py
@@ -24,9 +24,28 @@ async def upload_shapefile(file: UploadFile):
     # Now you can work with the SHP file using libraries like geopandas and shapely.
     
     # Example: Reading shapefile and extracting some data
-    # import geopandas as gpd
-    # data = gpd.read_file(file.filename)
-    # feature = data.iloc[0]  # Get the first feature
-    # geometry_type = shape(feature['geometry']).geom_type
+    import geopandas as gpd
+    import fiona
+    import zipfile
+    import StringIO
+    import shapefile
+    
+    fiona.drvsupport.supported_drivers['kml'] = 'rw' # enable KML support which is disabled by default
+    fiona.drvsupport.supported_drivers['KML'] = 'rw' # enable KML support which is disabled by default
 
-    # return {"name": file.filename, "geometry_type": geometry_type}
+
+    zipshape = zipfile.ZipFile(open(r'C:\GIS\Temp\RoadsShapefileFolder.zip', 'rb'))
+    print(zipshape.namelist())
+    dbfname, _, shpname, _, shxname = zipshape.namelist()
+    r = shapefile.Reader(shp=StringIO.StringIO(zipshape.read(shpname)),
+                        shx=StringIO.StringIO(zipshape.read(shxname)),
+                        dbf=StringIO.StringIO(zipshape.read(dbfname)))
+
+    print(r.bbox)
+    print(r.numRecords)
+
+    data = gpd.read_file(file.filename)
+    feature = data.iloc[0]  # Get the first feature
+    geometry_type = shape(feature['geometry']).geom_type
+
+    return {"name": file.filename, "geometry_type": geometry_type}
