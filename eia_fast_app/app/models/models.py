64f74f41b7f53abc1006e226952188f584e715a2
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-11-01 19:44:24 +0100

    Create models.py

---
+++ eia_fast_app/app/models/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from geoalchemy2 import Geometry
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class GeoSpatialData(Base):
+    __tablename__ = "input_geospatial_data"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    crs = 3765
+    geometry = Column(Geometry("GEOMETRY", srid=crs))
