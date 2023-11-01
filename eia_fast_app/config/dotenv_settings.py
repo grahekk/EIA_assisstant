9e512f488c3287b72e608221e88e5e135c2bea36
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-11-01 19:44:54 +0100

    Create dotenv_settings.py

---
+++ eia_fast_app/config/dotenv_settings.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Define settings using os.getenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+db_name = os.getenv("db_name")
+username = os.getenv("username")
+password = os.getenv("password")
+host = os.getenv("host")
+port = os.getenv("port")
