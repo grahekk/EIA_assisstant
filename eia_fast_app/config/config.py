a721d62c458c59e90a4ffb09ec73842336057257
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-11-01 20:39:06 +0100

    Update config.py

--- eia_fast_app/config/config.py
+++ eia_fast_app/config/config.py
@@ -1,32 +1,46 @@
 from pydantic_settings import BaseSettings
-from dotenv_settings import port, host, username, password, db_name
+from .dotenv_settings import port, host, username, password, db_name
+
+config_args = {"port": port, 
+               "host": host,
+               "username": username, 
+                "db_name": db_name,
+                "password": password
+}
+
+database_url: str = f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
+
+# class LocalSettings(BaseSettings):
+#     env_name: str = "Local"
+#     port: int
+#     host: str
+#     username: str
+#     db_name: str
+#     password: str
+
+#     @property
+#     def database_url(self):
+#         return f"postgresql://{self.username}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+
+#     class Config:
+#         env_file = ".env"
+
+# class Settings(BaseSettings):
+#     local: LocalSettings
+#     env_name: str = "Local"
+
+#     @property
+#     def database_url(self):
+#         return self.local.database_url
+
+#     class Config:
+#         env_file = ".env"
 
 
-class LocalSettings(BaseSettings):
-    env_name: str = "Local"
-    port: int = port
-    host: str = host
-    username: str = username
-    db_name: str = db_name
-    password: str = password
-    database_url: str = f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
-
-    class Config:
-        env_file = ".env"
-
-class Settings(BaseSettings):
-    local: LocalSettings
-    env_name: str = "Local"
-    database_url = LocalSettings.database_url
-
-    class Config:
-        env_file = ".env"
+# def get_settings() -> Settings:
+#     settings = Settings()
+#     print(f"Loading settings for: {settings.env_name}")
+#     return settings
 
 
-def get_settings() -> Settings:
-    settings = Settings()
-    print(f"Loading settings for: {settings.env_name}")
-    return settings
-
-
-settings = get_settings()
+# settings = get_settings()
