8e37adb146fd89b4e0e77642bed32fd5c8ee6886
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-11-01 19:44:48 +0100

    Update config.py

--- eia_fast_app/config/config.py
+++ eia_fast_app/config/config.py
@@ -1,12 +1,27 @@
 from pydantic_settings import BaseSettings
+from dotenv_settings import port, host, username, password, db_name
 
-class Settings(BaseSettings):
+
+class LocalSettings(BaseSettings):
     env_name: str = "Local"
-    base_url: str = "http://localhost:8000"
-    db_url: str = "sqlite:///./eia_app.db"
+    port: int = port
+    host: str = host
+    username: str = username
+    db_name: str = db_name
+    password: str = password
+    database_url: str = f"postgresql://{username}:{password}@{host}:{port}/{db_name}"
 
     class Config:
         env_file = ".env"
+
+class Settings(BaseSettings):
+    local: LocalSettings
+    env_name: str = "Local"
+    database_url = LocalSettings.database_url
+
+    class Config:
+        env_file = ".env"
+
 
 def get_settings() -> Settings:
     settings = Settings()
@@ -14,4 +29,4 @@ def get_settings() -> Settings:
     return settings
 
 
-# get_settings().db_url
+settings = get_settings()
