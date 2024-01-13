215913b747a3efd09b545c5bbe38128a817718ab
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2024-01-13 10:25:14 +0100

    connect with postgres
    new database url to connect with postgres

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+db_name = os.getenv("db_name")
+user = os.getenv("user")
+password = os.getenv("password")
+host = os.getenv("host")
+port = os.getenv("port")
+database_url: str = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = database_url or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 20
