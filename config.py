576f7c491e56a362af33d23d95fdbce7ef30342b
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2024-03-30 18:59:58 +0100

    Update config.py
    text templates are loaded in the class from config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import yaml
 
 load_dotenv()
 
@@ -12,6 +13,7 @@ host = os.getenv("host")
 port = os.getenv("port")
 database_url: str = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 # database_url: str = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}"
+yaml_templates = 'app\text_templates.yaml'
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
@@ -19,3 +21,21 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 20
+
+
+class TextTemplates():
+    """
+    Represents text templates from yaml file.
+    """
+    text_templates = yaml.safe_load(yaml_templates)
+
+
+def get_text_templates():
+    """
+    Retrieve the text_templates object that holds yaml attributes.
+
+    Returns:
+        text_templates object: The text_templates object that holds various strings and texts for app.
+
+    """
+    return TextTemplates().text_templates
