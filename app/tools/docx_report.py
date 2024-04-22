680172bf6304212d783233950868fb9a92cf6df5
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2024-04-22 22:27:03 +0200

    Fixes for updates of template

--- app/tools/docx_report.py
+++ app/tools/docx_report.py
@@ -1,10 +1,55 @@
 from docx import Document
-from docx.shared import Pt
+from docx.shared import Pt, Inches
+from docx.enum.table import WD_TABLE_ALIGNMENT
 
-def update_existing_section(doc):
+def update_existing_section(doc, project, chapter):
     # Access the first paragraph and modify its text and formatting
     first_paragraph = doc.paragraphs[0]
-    first_paragraph.text = 'Updated Text'
+    first_paragraph.text = project.project_title
+
+    map_paragraph = doc.paragraphs[10]
+    # Remove the existing paragraph
+    map_paragraph.text = ""
+    r = map_paragraph.add_run()
+    r.add_picture('map_image.jpg', width=Inches(9.0))
+
+    if chapter.table == None:
+        pass
+    else:
+        # Add a table in place of the existing table
+        table = doc.tables[0]
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        for col in range(len(chapter.table_columns)):
+            print(chapter.table_columns[col])
+            table.cell(0, col).text = chapter.table_columns[col]
+        for row_data in chapter.table[0:len(chapter.table)-1]:
+            row = table.add_row().cells
+
+        for i, row in enumerate(chapter.table, start=1):
+            for col in range(len(chapter.table_columns)):
+                try:
+                    if i == len(chapter.table):
+                        break
+                    if str(row[col]) == 'None':
+                        continue
+                    else:
+                        table.cell(i, col).text = str(row[col])
+                except TypeError as e:
+                    print(f"Skipping {i, row, col} because of {e}")
+
+
+    source_paragraph = doc.paragraphs[7]
+    source_paragraph.text = chapter.table_source
+    source_paragraph.style = "Izvor"
+
+    # navigating through paragraphs and document content
+    for i, para in enumerate(doc.paragraphs):
+        print(i, para.text)
+
+    for j, table in enumerate(doc.tables):
+        print(j, table)
+
 
 def add_chapter(doc, chapter):
     # Add chapter heading
@@ -33,9 +78,9 @@ def report_from_docx_template(project, report):
 
     # Iterate over project chapters
     for chapter in project.chapters:
-        if chapter.heading == "bla bla":
+        if chapter.heading == "Zaštićena područja Natura2000":
             # Update existing section if it's the "Natura chapter"
-            update_existing_section(doc, "A.1	PODACI O EKOLOŠKOJ MREŽI", chapter.description)
+            update_existing_section(doc, project, chapter)
         else:
             # Add a new chapter
             add_chapter(doc, chapter)
@@ -47,19 +92,18 @@ def report_from_docx_template(project, report):
 def add_chapters_table(doc, chapter):
     table = doc.add_table(rows=len(chapter.table)+1, cols=len(chapter.table_columns))
     table.style = 'DV_tablica'
-    table.autofit = False
+    table.autofit = True
     table.allow_autofit = False
+    table.alignment = WD_TABLE_ALIGNMENT.CENTER
     for row in table.rows:
         for cell in row.cells:
             cell.width = Pt(100)
     for col in range(len(chapter.table_columns)):
         table.cell(0, col).text = chapter.table_columns[col]
 
-
     for i, row in enumerate(chapter.table, start=1):
         for col in range(len(chapter.table_columns)):
-            table.cell(i, col).text = row[col]
-
-            # table.cell(i, 0).text = row[0]
-            # table.cell(i, 1).text = row[1]
-            # table.cell(i, 2).text = row[2]
+            try:
+                table.cell(i, col).text = str(row[col])
+            except TypeError as e:
+                print(f"Skipping {i, row, col} because of {e}")
